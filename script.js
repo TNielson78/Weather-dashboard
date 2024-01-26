a67a294bcade41f7a25ae9f22b3fef1f450132b3
TNielson78 <travis.nielson01@gmail.com>
2024-01-26 12:24:14 -0700

    finish 5 day forecast

--- script.js
+++ script.js
@@ -1,113 +1,128 @@
-var searchHistory = []
+let searchedCities = JSON.parse(localStorage.getItem('search-history')) || []
+// User specific api Key..
 var API = '330f80ef281593b850548bb6ba30d4cf'
 var searchBtn = document.getElementById('searchBtn')
 var fiveDayWeatherEl = document.querySelector(".five-day-weather")
 function getGeolocation(city) {
   var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API}`
   fetch(geoUrl).then(response => response.json()).then(data => {
-    console.log(data)
+
     var lat = data[0].lat
     var lon = data[0].lon
     var cityName = data[0].name
-    if (searchHistory.indexOf(cityName) === -1) {
-      searchHistory.push(cityName)
-      localStorage.setItem('search-history', JSON.stringify(searchHistory))
+    if (searchedCities.indexOf(cityName) === -1) {
+      searchedCities.push(cityName)
+      localStorage.setItem('search-history', JSON.stringify(searchedCities))
     }
 
     getWeather(lat, lon)
   })
 }
+// Lat and Lon added so weather can be city specific
 function getWeather(lat, lon) {
+  document.getElementById('current-weather').innerHTML = ''
   var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API}`
   fetch(weatherUrl).then(response => response.json()).then(data => {
     console.log(data)
-    console.log('success')
-  })
+    var temp = data.main.temp
+    var ptemp = document.createElement('p')
+    ptemp.textContent = 'temperature: ' + temp;
+    var humidity = data.main.humidity
+    var phum = document.createElement('p')
+    phum.textContent = 'humidity: ' + humidity;
+    var windspeed = data.wind.speed
+    var pwind = document.createElement('p')
+    pwind.textContent = 'windspeed: ' + windspeed
 
+    console.log(windspeed)
+    document.getElementById('current-weather').append(ptemp, phum, pwind)
+    document.getElementById('current-city').textContent = data.name
+    displaySearchHistory()
+  })
+  getforecast(lat, lon)
 }
+function getforecast(lat, lon) {
+  document.querySelector('.five-day-weather').innerHTML =''
+  var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`
+  fetch(forecastUrl).then(response => response.json()).then(data => {
+    console.log(data)
+    var forecastarray = []
+    for (var i = 0; i < data.list.length; i++) {
+      var time = data.list[i].dt_txt.split(' ')[1]
+      if (time === '12:00:00') {
+        forecastarray.push(data.list[i])
+      }
+    }
+    console.log(forecastarray)
+    for (var i = 0; i < forecastarray.length; i++) {
+      var temp = forecastarray[i].main.temp
+      var ptemp = document.createElement('p')
+      ptemp.textContent = 'temperature: ' + temp;
+      var humidity = forecastarray[i].main.humidity
+      var phum = document.createElement('p')
+      phum.textContent = 'humidity: ' + humidity;
+      var windspeed = forecastarray[i].wind.speed
+      var pwind = document.createElement('p')
+      pwind.textContent = 'windspeed: ' + windspeed
+      var cardDiv = document.createElement('div')
+      cardDiv.setAttribute('class', 'card xm-5')
+      cardDiv.setAttribute('style', "width: 13rem;")
+      var cardBody = document.createElement('div')
+      cardBody.setAttribute('class', 'card-body')
+      var cardTitle = document.createElement('h5')
+      cardTitle.setAttribute('class', 'card-title')
+      cardBody.append(cardTitle)
+      cardBody.append(ptemp, phum, pwind)
+      cardDiv.append(cardBody)
+      document.querySelector('.five-day-weather').append(cardDiv)
+    }
+
+
+
+
+    // console.log(windspeed)   
+    // document.getElementById('current-weather').append(ptemp,phum,pwind)
+    // document.getElementById('current-city').textContent= data.name
+
+  })
+}
+// Button added that also logs previous city and displays
 searchBtn.addEventListener('click', function (event) {
   var cityName = document.getElementById('cityInput').value
   getGeolocation(cityName)
-  displaySearchHistory()
+  // displaySearchHistory()
 })
 
 
-let searchedCities = JSON.parse(localStorage.getItem('search-history')) || []
+
 
 
 // Function to display weather data
-function displayWeatherData(city, weatherData) {
-  const weatherContainer = document.getElementById('weatherContainer');
-  const cityElement = document.createElement('div');
-  cityElement.innerHTML = `<h2>${city}</h2>`;
-  // Extract and display relevant weather information here
-  // For example: cityElement.innerHTML += `<p>Temperature: ${weatherData.main.temp}°C</p>`;
-  weatherContainer.appendChild(cityElement);
-}
-// for (const city of searchedCities) {
-//   fetchWeatherData(city)
-//     .then((weatherData) => {
-//       if (weatherData) {
-//         displayWeatherData(city, weatherData);
-//       }
-//     });
-// }
-function displaySearchHistory() { 
-  var searchHistory=JSON.parse(localStorage.getItem('search-history'))
-  var searchHistoryContainer=document.getElementById("search-history")
-  searchHistoryContainer.innerHTML=""
-  console.log(searchHistory)
-  for (var i=0; i< searchHistory.length; i++){
-    var button=document.createElement("button")
-    button.textContent=searchHistory[i]
+// function displayWeatherData(city, weatherData) {
+//   const weatherContainer = document.getElementById('weatherContainer');
+//   const cityElement = document.createElement('div');
+//   cityElement.innerHTML = `<h2>${city}</h2>`;
+//   weatherContainer.appendChild(cityElement);
+
+// 
+
+function displaySearchHistory() {
+  // var searchHistory = JSON.parse(localStorage.getItem('search-history'))
+  var searchHistoryContainer = document.getElementById("search-history")
+  searchHistoryContainer.innerHTML = ""
+  for (var i = 0; i < searchedCities.length; i++) {
+    var button = document.createElement("button")
+    button.textContent = searchedCities[i]
+    button.setAttribute('value', searchedCities[i])
+    button.addEventListener('click', function () {
+      getGeolocation(this.value)
+      console.log(this)
+    })
     searchHistoryContainer.appendChild(button)
   }
 }
 displaySearchHistory()
 
-
-// Event listener for the search button
-// document.getElementById('searchBtn').addEventListener('click', async () => {
-//   const cityInput = document.getElementById('cityInput');
-//   const city = cityInput.value;
-//   if (city) {
-//     const weatherData = await fetchWeatherData(city);
-//     if (weatherData) {
-//       searchedCities.push(city);
-//       displayWeatherData(city, weatherData);
-//     }
-//   }
-// });
-
-// Loop through previously searched cities and display weather data
-
-
-// Test code for current weather conditions.........
-// const apiKey = '330f80ef281593b850548bb6ba30d4cf'
-// const city = 'city'; // Replace with the desired city
-
-// const weatherContainer = document.getElementById('weather-container');
-
-// // Api request
-// const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`;
-// fetch(weatherUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     // Extract relevant weather information from the API response
-//     const temperature = data.main.temp;
-//     const conditions = data.weather[0].description;
-
-//     // weather container update function
-//     weatherContainer.innerHTML = `
-//   <h2>Current Weather in ${city}</h2>
-//   <p>Temperature: ${temperature}°C</p>
-//   <p>Conditions: ${conditions}</p>
-//   `;
-//   })
-//   .catch(error => {
-//     console.error('Error fetching weather data:', error);
-//     weatherContainer.innerHTML = 'Error fetching weather data';
-//   });
 
 
 
